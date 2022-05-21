126a3a50f48d51e27b13961af19abbc600316ece
jiyoungKim <rlawldud.kim99@gmail.com>
2022-05-22 00:39:05 +0900

    #6068

---
+++ 6068.py
@@ -0,0 +1,11 @@
+#2021.01.07 Code-Up 100제
+n = int(input())
+
+if (n >= 90 and n <= 100):
+    print("A")
+elif (n >= 70 and n <= 89):
+    print("B")
+elif (n >= 40 and n <= 69):
+    print("C")
+else:
+    print("D")
