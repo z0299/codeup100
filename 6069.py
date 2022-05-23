279a5303234d19f0fb0cbfc5318962e4d1146271
jiyoungKim <rlawldud.kim99@gmail.com>
2022-05-23 15:44:43 +0900

    #6069

---
+++ 6069.py
@@ -0,0 +1,13 @@
+#2021.01.07 Code-Up 100제
+c = input()
+
+if(c == 'A'):
+    print("best!!!")
+elif(c == 'B'):
+    print("good!!")
+elif(c == 'C'):
+    print("run!")
+elif(c == 'D'):
+    print("slowly~")
+else:
+    print("what?")
