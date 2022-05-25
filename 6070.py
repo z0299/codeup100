580b82c8e70421fd7376de323b19570d89d344bc
jiyoungKim <rlawldud.kim99@gmail.com>
2022-05-25 16:38:02 +0900

    #6070

---
+++ 6070.py
@@ -0,0 +1,11 @@
+#2021.01.07 Code-Up 100제
+m = int(input())
+
+if(m == 12 or m == 1 or m == 2):
+    print("winter")
+elif(m == 3 or m == 4 or m == 5):
+    print("spring")
+elif(m == 6 or m == 7 or m == 8):
+    print("summer")
+else:
+    print("fall")
